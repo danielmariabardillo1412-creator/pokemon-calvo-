22deb1dfb24dcdb8ba214caf639ac94a598b0ebc
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-08-31 00:51:17 +0200

    test(data-v3): add provenance domain runner

---
+++ tests/data_foundation_v3_domain_test_runner.gd
@@ -0,0 +1,23 @@
+extends SceneTree
+
+var _passed := 0
+var _failed := 0
+
+
+func _initialize() -> void:
+	call_deferred("_run")
+
+
+func _run() -> void:
+	DataFoundationV3DomainTestSuite.new().run(Callable(self, "_check"))
+	print("\n=== DATA FOUNDATION V3 DOMAIN RESULT: %d PASS / %d FAIL ===" % [_passed, _failed])
+	quit(0 if _failed == 0 else 1)
+
+
+func _check(name: String, condition: bool) -> void:
+	if condition:
+		_passed += 1
+		print("PASS  %s" % name)
+	else:
+		_failed += 1
+		push_error("FAIL  %s" % name)
