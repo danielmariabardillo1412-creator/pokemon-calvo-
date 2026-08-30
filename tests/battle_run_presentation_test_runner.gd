3dda355f02fad4be388b1b983611a64806c186ae
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-08-30 09:47:57 +0200

    test: add battle run presentation runner

---
+++ tests/battle_run_presentation_test_runner.gd
@@ -0,0 +1,25 @@
+extends SceneTree
+
+var _passed := 0
+var _failed := 0
+
+
+func _initialize() -> void:
+	call_deferred("_run_all")
+
+
+func _run_all() -> void:
+	var suite := BattleRunPresentationTestSuite.new()
+	await suite.run(_check, self)
+	print("")
+	print("=== BATTLE RUN PRESENTATION RESULT: %d PASS / %d FAIL ===" % [_passed, _failed])
+	quit(0 if _failed == 0 else 1)
+
+
+func _check(label: String, condition: bool) -> void:
+	if condition:
+		_passed += 1
+		print("PASS  %s" % label)
+	else:
+		_failed += 1
+		print("FAIL  %s" % label)
