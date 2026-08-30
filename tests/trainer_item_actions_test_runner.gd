4553e6e958b149968031da07a27af5a837ed83c6
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-08-30 20:20:10 +0200

    FASE30 run corrected item fixture suite

--- tests/trainer_item_actions_test_runner.gd
+++ tests/trainer_item_actions_test_runner.gd
@@ -9,7 +9,7 @@ func _initialize() -> void:
 
 
 func _run() -> void:
-	TrainerItemActionsTestSuite.new().run(Callable(self, "_check"))
+	TrainerItemActionsV2TestSuite.new().run(Callable(self, "_check"))
 	print("\n=== TRAINER ITEM ACTIONS RESULT: %d PASS / %d FAIL ===" % [_passed, _failed])
 	quit(0 if _failed == 0 else 1)
 
