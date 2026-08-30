a73ed3d9b6c71d9649fda06386ce3959f4e9423f
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-08-30 20:31:12 +0200

    FASE31 add strategic switching corpus runner

---
+++ tests/trainer_strategic_switching_v2_corpus_test_runner.gd
@@ -0,0 +1,23 @@
+extends SceneTree
+
+var _passed: int = 0
+var _failed: int = 0
+
+
+func _initialize() -> void:
+	call_deferred("_run")
+
+
+func _run() -> void:
+	TrainerStrategicSwitchingV2CorpusTestSuite.new().run(Callable(self, "_check"))
+	print("\n=== TRAINER STRATEGIC SWITCHING V2 CORPUS RESULT: %d PASS / %d FAIL ===" % [_passed, _failed])
+	quit(0 if _failed == 0 else 1)
+
+
+func _check(name: String, condition: bool) -> void:
+	if condition:
+		_passed += 1
+		print("PASS  %s" % name)
+	else:
+		_failed += 1
+		push_error("FAIL  %s" % name)
