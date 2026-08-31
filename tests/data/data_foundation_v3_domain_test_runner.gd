f9d3538dec9c443e60070b0e4bf7d7904c984e55
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-01 01:49:09 +0200

    tests: run move-property ability suite in DATA V3

--- tests/data/data_foundation_v3_domain_test_runner.gd
+++ tests/data/data_foundation_v3_domain_test_runner.gd
@@ -26,6 +26,7 @@ func _run() -> void:
 	DataFoundationV3AbilityHitStatTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityContactDamageTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityDefensiveDamageTestSuite.new().run(Callable(self, "_check"))
+	DataFoundationV3AbilityMovePropertyTestSuite.new().run(Callable(self, "_check"))
 	print("\n=== DATA FOUNDATION V3 DOMAIN RESULT: %d PASS / %d FAIL ===" % [_passed, _failed])
 	quit(0 if _failed == 0 else 1)
 
