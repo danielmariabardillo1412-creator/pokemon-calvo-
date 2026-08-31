7083798b74792fdd5914633142b1332617b19594
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-08-31 21:57:31 +0200

    fix(data-v3): append terminal user audit layer

--- tools/pokeapi_adapter_user_audit_chain.py
+++ tools/pokeapi_adapter_user_audit_chain.py
@@ -5,10 +5,12 @@ from __future__ import annotations
 import pokeapi_adapter_user_hp_cost as user_hp_cost
 import pokeapi_adapter_user_mandatory_state as user_mandatory_state
 import pokeapi_adapter_user_persistent_state as user_persistent_state
+import pokeapi_adapter_user_terminal_state as user_terminal_state
 
 
 def apply_user_audits(move: dict, generated: tuple):
     audited = user_hp_cost.apply_user_hp_cost(move, generated)
     audited = user_mandatory_state.apply_user_mandatory_state(move, audited)
     audited = user_persistent_state.apply_user_persistent_state(move, audited)
+    audited = user_terminal_state.apply_user_terminal_state(move, audited)
     return audited
