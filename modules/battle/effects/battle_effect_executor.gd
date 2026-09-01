c49d8174ae5992446baf4d26635149ae69ad7ae1
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-01 02:55:40 +0200

    battle: preserve calculator force-critical argument order

--- modules/battle/effects/battle_effect_executor.gd
+++ modules/battle/effects/battle_effect_executor.gd
@@ -203,6 +203,7 @@ func _damage(
 		context.rng,
 		context.ruleset,
 		int(modifiers.multiplier_basis_points),
+		-1,
 		int(modifiers.offensive_stat_multiplier_basis_points),
 	)
 	if result.critical:
