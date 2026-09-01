9ef6305de6e11f789577e982fdc25decef25a5d6
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-01 12:35:46 +0200

    tests: register ability damage role suite

--- tests/data/data_foundation_v3_domain_test_runner.gd
+++ tests/data/data_foundation_v3_domain_test_runner.gd
@@ -28,6 +28,7 @@ func _run() -> void:
 	DataFoundationV3AbilityDefensiveDamageTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityOffensiveStatTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityOffensiveStatConditionsTestSuite.new().run(Callable(self, "_check"))
+	DataFoundationV3AbilityDamageRoleTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityMovePropertyTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityContactReactionTestSuite.new().run(Callable(self, "_check"))
 	DataFoundationV3AbilityContactRetaliationTestSuite.new().run(Callable(self, "_check"))
