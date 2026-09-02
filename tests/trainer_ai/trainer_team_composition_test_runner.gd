6650355933f3d719354b6721de9549b34950f124
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-03 00:42:43 +0200

    test(trainer-ai): human checkpoint C3f-o

--- tests/trainer_ai/trainer_team_composition_test_runner.gd
+++ tests/trainer_ai/trainer_team_composition_test_runner.gd
@@ -9,7 +9,7 @@ func _initialize() -> void:
 
 
 func _run() -> void:
-	TrainerRosterSearchSwitchSamplingBoundaryAuditTestSuite.new().run(Callable(self, "_check"))
+	TrainerRosterSearchSwitchSamplingStrategyComparisonAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralOverlapRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralFormulaComparisonTestSuite.new().run(Callable(self, "_check"))
