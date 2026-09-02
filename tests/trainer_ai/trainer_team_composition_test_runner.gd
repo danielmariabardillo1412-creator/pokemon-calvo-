78e2d7cf535c45b9bb92c24e0247195fd19d1faf
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-02 16:27:34 +0200

    test(trainer-ai): run C3f operational evidence audit

--- tests/trainer_ai/trainer_team_composition_test_runner.gd
+++ tests/trainer_ai/trainer_team_composition_test_runner.gd
@@ -9,7 +9,7 @@ func _initialize() -> void:
 
 
 func _run() -> void:
-	TrainerRosterStructuralValueProductionTestSuite.new().run(Callable(self, "_check"))
+	TrainerRosterOperationalEvidenceAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralOverlapRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralFormulaComparisonTestSuite.new().run(Callable(self, "_check"))
