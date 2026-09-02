18ef333f67fcc0bdfed2862605a69f1c3416f0c4
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-02 14:31:03 +0200

    test(trainer-ai): probe eager disjoint helper initialization

--- tests/trainer_ai/trainer_roster_structural_registry_probe.gd
+++ tests/trainer_ai/trainer_roster_structural_registry_probe.gd
@@ -1,2 +1,4 @@
 class_name TrainerRosterStructuralRegistryProbe
 extends TrainerRosterStructuralRealDataAuditTestSuite
+
+var _disjoint_helper := TrainerRosterStructuralOverlapRealDataAuditTestSuite.new()
