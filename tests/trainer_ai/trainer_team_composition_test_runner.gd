52f03b03285777d926e001e69860303b6e25fe9c
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-04 18:35:34 +0200

    test(trainer-ai): human checkpoint C3f-ad disjoint ItemAware margin

--- tests/trainer_ai/trainer_team_composition_test_runner.gd
+++ tests/trainer_ai/trainer_team_composition_test_runner.gd
@@ -9,7 +9,7 @@ func _initialize() -> void:
 
 
 func _run() -> void:
-	TrainerRosterSearchItemAwareDepth1SwitchScorePortabilityAuditTestSuite.new().run(Callable(self, "_check"))
+	TrainerRosterSearchItemAwareMarginDisjointRoleLocalValidationAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralOverlapRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralFormulaComparisonTestSuite.new().run(Callable(self, "_check"))
