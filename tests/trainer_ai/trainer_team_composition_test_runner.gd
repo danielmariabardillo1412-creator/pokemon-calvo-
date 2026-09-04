4af64cd1b6e4ab3d6a61f4b2b899f1d2538ad959
danielmariabardillo1412-creator <danielmariabardillo1412@gmail.com>
2026-09-04 03:49:29 +0200

    test(trainer-ai): technical checkpoint C3f-ac

--- tests/trainer_ai/trainer_team_composition_test_runner.gd
+++ tests/trainer_ai/trainer_team_composition_test_runner.gd
@@ -9,7 +9,7 @@ func _initialize() -> void:
 
 
 func _run() -> void:
-	TrainerRosterSearchSymmetricPublicHistoryBranchPerspectiveAuditTestSuite.new().run(Callable(self, "_check"))
+	TrainerRosterSearchItemAwareDepth1SwitchScorePortabilityAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralOverlapRealDataAuditTestSuite.new().run(Callable(self, "_check"))
 	TrainerRosterStructuralFormulaComparisonTestSuite.new().run(Callable(self, "_check"))
